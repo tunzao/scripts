3f721b13d586bb99b0adbc3ced07c979977b6ac6
tunzao <l.tunzao@gmail.com>
2014-02-27 19:42:23 +0800

    add findkill.sh

---
+++ findkill.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# kill 占用某个端口的进程
+if [ $# != 1 ]
+then
+    echo "usage: findkill.sh port_number"
+else
+    netstat -lnp | grep $1 | awk '{print $7}' | awk -F/ '{print $1}' | xargs kill
+fi
